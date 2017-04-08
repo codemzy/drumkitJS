96798ae2f9372ebcd4248049185def96390f6331
Emma J <codemzy@gmail.com>
2017-04-08 11:02:01 +0000

    Added play on key down if element exists

--- js/index.js
+++ js/index.js
@@ -3,5 +3,9 @@
 // listen for the keydown event
 window.addEventListener('keydown', function(e) {
     const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
-    console.log(audio);
+    // stop the function if no audio element
+    if (!audio) {
+        return;
+    }
+    audio.play();
 });
