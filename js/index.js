a0102544d2fa6e4fbc4435e0fa043b3b5697ffdd
Emma J <codemzy@gmail.com>
2017-04-08 11:08:41 +0000

    Added class to key element when matched

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@
 
 // listen for the keydown event
 window.addEventListener('keydown', function(e) {
-    const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+    const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]'); // get the audio element if matches keyCode
+    const key = document.querySelector('.key[data-key="' + e.keyCode + '"]'); // get the key element if matches keyCode
     // stop the function if no audio element
     if (!audio) {
         return;
     }
-    audio.play();
+    audio.currentTime = 0; // rewind if already playing
+    audio.play(); // play audio
+    key.classList.add('playing'); // add the class playing to the key element
 });
