eb9d8a11f015b9e778523c0f7625d26296b6c1fd
Emma J <codemzy@gmail.com>
2017-04-08 12:03:49 +0000

    Added click event listener to get data-value

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
-// listen for the keydown event
-window.addEventListener('keydown', function(e) {
+// function to play the sound
+function playSound(e) {
     const audio = document.querySelector('audio[data-key="' + e.keyCode + '"]'); // get the audio element if matches keyCode
     const key = document.querySelector('.key[data-key="' + e.keyCode + '"]'); // get the key element if matches keyCode
     // stop the function if no audio element
@@ -11,8 +11,9 @@ window.addEventListener('keydown', function(e) {
     audio.currentTime = 0; // rewind if already playing
     audio.play(); // play audio
     key.classList.add('playing'); // add the class playing to the key element
-});
+}
 
+// function to remove playing class when transition finished
 function removeTransition(e) {
     if (e.propertyName !== 'transform') {
         return; // skip if not a transform
@@ -20,8 +21,14 @@ function removeTransition(e) {
     this.classList.remove('playing'); // remove the class playing from the key element
 }
 
+// listen for the keydown event
+window.addEventListener('keydown', playSound);
+
 // when the transition ends remove the class
 const keys = document.querySelectorAll('.key');
 keys.forEach(function(key) {
     key.addEventListener('transitionend', removeTransition);
+    key.addEventListener('click', function(e) {
+        console.log(this.attributes['data-key'].value);
+    });
 });
